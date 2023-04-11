b5db0317947550cab2770dfe203611371bcb56d2
amzaghiles91 <amzaghiles91@gmail.com>
2023-04-10 23:04:32 -0400

    second term

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +14,44 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		List<String> list = new ArrayList<>();
 
+		// Add elements to the ArrayList
+		list.add("apple");
+		list.add("banana");
+		list.add("cherry");
+		list.add("date");
+		list.add("elderberry");
+
+		// Retrieve elements using peek
+		System.out.println("Peek: " + list.get(2));
+
+		// Remove an element from the ArrayList
+		list.remove(3);
+
+		// Retrieve elements using for-each loop
+		System.out.println("Using for-each loop:");
+		for (String element : list) {
+			System.out.println(element);
+		}
+
+		// Retrieve elements using while loop with Iterator
+		System.out.println("Using while loop with Iterator:");
+		Iterator<String> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+		// Sort the elements in the ArrayList
+		Collections.sort(list);
+
+		// Store the sorted data into a database
+		// (Code to store data in a database would go here)
+		System.out.println("Sorted data has been stored in a database");
 	}
-
 }
+
+
+
+
+
