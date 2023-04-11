7ba4b34e27e510f4e779aa8f234f499f9e570d79
amzaghiles91 <amzaghiles91@gmail.com>
2023-04-11 00:10:58 -0400

    second term

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -9,7 +12,40 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        // Remove punctuation marks and convert to lowercase
+        st = st.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
 
+        // Split the string into words
+        String[] words = st.split(" ");
+
+        // Calculate the average length of the words
+        int sum = 0;
+        for (String word : words) {
+            sum += word.length();
+        }
+        double averageLength = (double) sum / words.length;
+
+        // Find duplicate words and their number of occurrences
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+
+        // Print the results
+        System.out.println("Duplicate words and their number of occurrences:");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+        System.out.println("Average length of the words: " + averageLength);
     }
-
 }
+
+
+
+
