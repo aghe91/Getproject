a5bd9e987498a632fe5ac9e36816d4eb331c583b
amzaghiles91 <amzaghiles91@gmail.com>
2023-04-10 22:43:03 -0400

    second term

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
@@ -11,8 +13,30 @@ public class PrimeNumber {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
-		 */
+		 */        boolean[] isPrime = new boolean[1000001];
+		Arrays.fill(isPrime, true);
 
+		for (int i = 2; i * i <= 1000000; i++) {
+			if (isPrime[i]) {
+				for (int j = i * i; j <= 1000000; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+
+		int count = 0;
+		for (int i = 2; i <= 1000000; i++) {
+			if (isPrime[i]) {
+				count++;
+				// Store or print the prime number here
+			}
+		}
+
+		System.out.println("Number of prime numbers between 2 and 1 million: " + count);
 	}
-
 }
+
+
+
+
+
